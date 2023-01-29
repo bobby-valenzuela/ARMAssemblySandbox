34aff124910dbf3ff5ef569339c0f1e0524e3271
Bobby Valenzuela <bobby.valenzuela@protonmail.com>
2023-01-29 10:33:06 -0800

    Added hello world code

--- hello_world.asm
+++ hello_world.asm
@@ -1,20 +1,27 @@
 .section .data
 # Constants (non-executable data)
-my_list:
-	.word 14,5,6,7,8
+
+	greeting:
+		.ascii "Welcome to the program!\n" 
+
 .section .text
 # Start interpreting as code from this line
 
-.global _start
-# First run our "_start" function once we find it
-	
-_start:
-	# Main Program	
-	LDR R0,=my_list
-	MOV R1,#25 		# Place 25 (Base 10 - DEC) in R1
-	MOV R2,#0x19	# Place 25 (Base 16 - HEX) in R2
-	MOV R7,#1
+	.global _start
+	# First run our "_start" function once we find it
+		
+	_start:
+		# Main Program	
+		MOV R7, #0x4		
+		MOV R0, #1			
+		LDR R1, =greeting	
+		MOV R2, #23
 
-# compile on native: 
+		# Terminate/Exit
+		MOV R7, #0x1
+		MOV R0, #0
+		SWI 0
+
+# compile on native ARM Architecture: 
 # as 001.asm -o 001.o
 # gcc 001.o -o 001.elf -nostdlib
